4612a8571236aac56567efab6127718f56c1fe44
Ornella Sultanbekova <10b_sultanbekova@mail.ru>
2024-11-15 19:58:01 +0300

    func for simple number

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+def simple_num(n: int) -> bool:
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0: return False
+    return True
+
+num = int(input("Введите число для проверки на его простоту: "))
+print(simple_num(num))
